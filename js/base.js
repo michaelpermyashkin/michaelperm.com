63ba23aa8b9cec6425509127b0dbc969477e3289
Michael <mpermyashkin@gmail.com>
2021-02-01 19:49:23 -0500

    Added projects. Still need to add appropriate demos and screenshots

--- js/base.js
+++ js/base.js
@@ -1,4 +1,22 @@
+$(document).ready(function() {
+    // Gets anchor from URL -> #versions or #manage or #statistics
+    // used to set the correct div as visible when the page is reloaded
+    // So when a user refreshes the page, it will keep them at the same place within the manage page
+    var anchor = window.location.href.substring(window.location.href.indexOf("#") + 1);
+    if (anchor === "music") {
+        $('.music-link').click();
+    } else if (anchor === "portfolio") {
+        $('.portfolio-link').click();
+    } else if (anchor === "contact") {
+        $('.contact-link').click();
+    } else {
+        $('.link-about').click();
+    }
+});
+
+
 $('.tab-link').click(function() {
+    var active = $(this);
     $('#menu li a').each(function() {
         $(this).removeClass('active');
     });
@@ -7,28 +25,25 @@ $('.tab-link').click(function() {
         $(this).hide();
     });
 
-    $(this).addClass('active');
-    var tab = $(this).attr('title');
+    active.addClass('active');
+    var tab = active.attr('title');
 
     if (tab === 'portfolio') {
         $('#portfolio-link').addClass('active');
         $('#page-title').text('Developer')
-        $('#portfolio').show();
+        $('#tab-portfolio').show();
     } else if (tab === 'music') {
         $('#music-link').addClass('active');
         $('#page-title').text('Artist');
-        $('#music').show();
+        $('#tab-music').show();
     } else if (tab === 'contact') {
         $('#contact-link').addClass('active');
         $('#page-title').text('Contact');
-        $('#contact').show();
+        $('#tab-contact').show();
     } else {
         $('#about-link').addClass('active');
         $('#page-title').text('Me');
-        $('#about').show();
+        $('#tab-about').show();
     }
 
-})
-
-
-console.log($('#page-title').text());
+});
